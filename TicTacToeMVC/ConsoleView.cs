71db396832d068af590aa1ff752cb10b88b1472e
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-05-19 15:39:52 +0100

    Before MVC

---
+++ TicTacToeMVC/ConsoleView.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToeMVC
+{
+    public class ConsoleView
+    {
+        public void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("");
+        }
+    }
+}
